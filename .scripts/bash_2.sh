be3524fee006c02a46d8cd0d3cc6437087bf1c76
eyes <eyes@macOS.local>
2024-10-23 20:30:17 +0530

    Updated .wezterm.lua & .zshrc

--- .scripts/bash_2.sh
+++ .scripts/bash_2.sh
@@ -6,7 +6,7 @@ touch ~/.hushlogin
 # macOS Cli
 defaults write com.apple.screencapture type jpg
 defaults write com.apple.Dock showhidden -bool TRUE && killall Dock
-defaults write com.apple.dock ResetLaunchPad -bool true; killall Dock
+defaults write com.apple.dock autohide-delay -float 0; killall Dock
 
 # Install tgpt
 curl -sSL https://raw.githubusercontent.com/aandrew-me/tgpt/main/install | bash -s /usr/local/bin
--- .wezterm.lua
+++ .wezterm.lua
@@ -8,14 +8,12 @@ config.automatically_reload_config = true
 config.check_for_updates = false
 bold_brightens_ansi_colors = true
 config.font = wezterm.font("JetBrainsMono Nerd Font")
-config.font_size = 14
+config.font_size = 20
 --config.line_height = 1.0
 config.color_scheme = 'Catppuccin Frappe' -- Catppuccin Frappe | Mocha | Macchiato, duskfox, Dracula (Official)
 config.enable_tab_bar = false
-config.allow_win32_input_mode = true
-config.default_prog = { 'pwsh.exe' }
-config.initial_cols = 134
-config.initial_rows = 33
+config.initial_cols = 160
+config.initial_rows = 40
 config.default_cursor_style = 'BlinkingBar'
 config.audible_bell = "SystemBeep"
 config.adjust_window_size_when_changing_font_size = true
@@ -24,17 +22,15 @@ config.notification_handling = "AlwaysShow"
 config.show_update_window = false
 config.term = "xterm-256color"
 config.window_decorations = "RESIZE" -- RESIZE, INTEGRATED_BUTTONS|RESIZE, TITLE, NONE
+config.window_background_opacity = 0.94
+config.macos_window_background_blur = 10
 
 -- Customize the command palette font size and appearance
 config.command_palette_font_size = 12
 config.command_palette_fg_color = "#ffffff"  -- Customize foreground color
 config.command_palette_bg_color = "#44475a"  -- Customize background color
---config.font_shaper = "Harfbuzz"
---config.harfbuzz_features = { 'zero', 'liga=0', 'clig=0', 'calt=0' }
-
---config.window_background_opacity = 0.97
---config.win32_system_backdrop = 'Acrylic' -- Acrylic, Mica, Tabbed
---config.macos_window_background_blur = 20
+config.font_shaper = "Harfbuzz"
+config.harfbuzz_features = { 'zero' }
 
 config.window_padding = {
     left = '0cell',
@@ -57,4 +53,3 @@ config.colors = {
   }
 
 return config
-
