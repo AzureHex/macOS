a6415e067883103655666b4e5e0feaa049328eb8
eyes <eyes@macOS.local>
2024-10-24 13:36:56 +0530

    Added & remove some brew apps

--- .scripts/bash_3.sh
+++ .scripts/bash_3.sh
@@ -6,11 +6,10 @@ brew install --cask font-jetbrains-mono-nerd-font
 brew install --cask font-josefin-sans
 
 # Homebrew Casks
-brew install arc betterdisplay inna keepassxc localsend obsidian raycast wezterm whatsapp zed zen-browser
+brew install iina localsend obsidian raycast wezterm whatsapp zed zen-browser
 
 # Homebrew Formulae
 brew install aria2 bat btop cbonsai cmatrix curl duf eza fastfetch ffmpeg figlet fish fzf gcc git grep lazygit lsd mailsy neovim nu nushell oh-my-posh speedtest-cli starship syncthing tmux wget yazi yt-dlp zoxide zsh-autocomplete zsh-autosuggestions zsh-syntax-highlighting
 
 # Install tgpt
 curl -sSL https://raw.githubusercontent.com/aandrew-me/tgpt/main/install | bash -s /usr/local/bin
-
